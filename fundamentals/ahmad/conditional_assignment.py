87284124a31eb51adb515363e1c954dd8c9004cc
Ahmad <ahmadatto@gmal.com>
2023-09-07 12:49:56 +0100

    my practıce

---
+++ fundamentals/ahmad/conditional_assignment.py
@@ -0,0 +1,51 @@
+
+
+
+#number=(input("Enter a number:"))
+#if  number > 0:
+ #   print("positive")
+#if number < 0:
+ #   print("negative")
+#if number == 0:
+ #   print("zero")
+
+
+#num1=input("Enter number 1: "))
+#num2=(input("Enter number 2: "))
+
+#if num1 > num2:
+ #   print("the larger number is: {num1}")
+#elif num2 > num1:
+ #   print("the larger number is: {num2}")
+#else:
+ #   print("both numbers are equal")
+
+##input_string = "alameen"
+#input("Enter a string")
+
+#if input_string.startswith("a"):
+ #   print("start with 'a'")
+#else:
+#    print("doesn't starts with'a'" )
+
+
+
+
+
+#num=int(input("Enter a number:"))
+
+#if num %2==0:
+ #   print("Even")
+#else:
+#    print("Odd")
+
+
+
+#grades=input("Enter your grade(A,B,C,D,F):")
+
+#if grades in["A","B","C"]:
+ #   print("pass")
+#elif grades in["D","F"]:
+#    print("Fail")
+#else:
+#    print("invalid")
