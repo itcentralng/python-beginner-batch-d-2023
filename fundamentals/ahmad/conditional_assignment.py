ff807ea8e74c543eb1658fede8ddd706e199ab71
Ahmad <ahmadatto@gmal.com>
2023-09-28 12:38:22 +0100

    assıgnment Ahmad

--- fundamentals/ahmad/conditional_assignment.py
+++ fundamentals/ahmad/conditional_assignment.py
@@ -1,3 +1,30 @@
+#reg_email=input("Enter your email" )
+#reg_password=input("Enter your password" )
+#reg_name=input("Enter your name" )
+
+#user={'email':reg_email, 'password':reg_password,'name': reg_name}
+
+
+#login_email=input("Enter your email")
+#login_password=input("Enter your password")
+
+#if (login_email == user.get("email")) and (login_password == user.get("password")):
+ #   print("Hi {}, welcome to Facebook!".format(user.get("name")))
+#elif login_email == user.get("email") and login_password != user.get("password"):
+ #   print("password not correct")
+#elif login_email != user.get("email") and login_password == user.get("password"):
+ #   print("email is not correct")
+#else:
+ #   print("Credentials are invalid")
+
+
+  
+
+  
+
+
+
+
 
 
 
@@ -20,7 +47,7 @@
 #else:
  #   print("both numbers are equal")
 
-##input_string = "alameen"
+#input_string = "alameen"
 #input("Enter a string")
 
 #if input_string.startswith("a"):
@@ -46,6 +73,23 @@
 #if grades in["A","B","C"]:
  #   print("pass")
 #elif grades in["D","F"]:
-#    print("Fail")
+ #   print("Fail")
 #else:
-#    print("invalid")
+ #   print("invalid")
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+
+#for number in numbers:
+ #   print(number)
+
+#for number in numbers:
+ #    if number %2==1:
+  #       print(number)
+
+
+for number in numbers:
+    if number == 3:
+
+
+
+
